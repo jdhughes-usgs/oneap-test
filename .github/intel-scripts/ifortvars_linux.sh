33ad00d8b90f344a8b407a197725955050fdf93c
Joseph D Hughes <jdhughes@usgs.gov>
2021-03-01 15:51:39 -0600

    add workflow

---
+++ .github/intel-scripts/ifortvars_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+#shellcheck disable=SC2010
+LATEST_VERSION=$(ls -1 /opt/intel/oneapi/compiler/ | grep -v latest | sort | tail -1)
+# shellcheck source=/dev/null
+source /opt/intel/oneapi/compiler/"$LATEST_VERSION"/env/vars.sh
+
+cd .github/intel-scripts
+ifort hello.f90 -o hello.ifort.out
+ls
+./hello.ifort.out
+
+icc hello.cpp -o hello.icc.out
+ls
+./hello.icc.out
---
+++ .github/intel-scripts/ifortvars_macos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+source /opt/intel/oneapi/setvars.sh
+
+cd .github/intel-scripts
+ifort hello.f90 -o hello.ifort.out
+ls
+./hello.ifort.out
+
+icc hello.cpp -o hello.icc.out
+ls
+./hello.icc.out
